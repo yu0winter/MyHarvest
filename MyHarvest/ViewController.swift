12c9347b69404b09f255993425261e870f88f1a3
NiuYulong <15011116026@163.com>
2017-09-02 22:56:31 +0800

    Initial Commit

---
+++ MyHarvest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHarvest
+//
+//  Created by NiuYulong on 2017/9/2.
+//  Copyright © 2017年 NiuYulong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
