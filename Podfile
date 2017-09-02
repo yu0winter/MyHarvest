b736cd4380280078634d851e31f38152e4bb9e86
NiuYulong <15011116026@163.com>
2017-09-02 23:56:55 +0800

    Modified by nyl 1.更新CocoaPods本地库 2.添加JSON处理类库

--- Podfile
+++ Podfile
@@ -7,11 +7,11 @@ platform :ios, "8.0"
 def shared_pods
     pod 'Alamofire', '~> 4.4.0' # AFNetworking Swift 版本
     pod 'SnapKit' # Masonry Swift 版本
-    pod 'SDWebImage'
-    pod 'FMDB'
+    pod 'SDWebImage'    # 图片缓存
+    pod 'FMDB'          # 数据库
     pod 'FMDBMigrationManager' #数据库迁移管理器
     pod 'Bugly' #腾讯Bugly崩溃收集
-
+    pod 'SwiftyJSON'    #GitHub 上最为开发者认可的 JSON 解析类
 end
 
 target 'MyHarvest' do
--- Pods/Target Support Files/Pods-MyHarvest/Pods-MyHarvest-frameworks.sh
+++ Pods/Target Support Files/Pods-MyHarvest/Pods-MyHarvest-frameworks.sh
@@ -94,6 +94,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/FMDBMigrationManager/FMDBMigrationManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
@@ -101,6 +102,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/FMDBMigrationManager/FMDBMigrationManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
