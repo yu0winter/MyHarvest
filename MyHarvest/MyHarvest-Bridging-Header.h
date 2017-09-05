b18b4e2ff9df7e6384e9ba0f6c29173538d52fd7
NiuYulong <15011116026@163.com>
2017-09-05 22:43:05 +0800

    Modified by nyl 1.swift 项目中引用OC库SDWebImage

---
+++ MyHarvest/MyHarvest-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+
+//#import "SDWebImage/UIView+WebChat.h"
+//#import "UIImageView+WebChat.h"
+#import "SDWebImage/SDWebImage-umbrella.h"
+//SDWebImage-umbrella.h
+
+
+#import "PrefixHeader.pch"
